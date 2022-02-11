9f29a399dd474afbe9fe0f00da5fba4ec1db6b12
Ed <edadeagbo@gmail.com>
2022-02-11 15:11:15 -0700

    added js folder/file

---
+++ js/bna.js
@@ -0,0 +1,20 @@
+const firstName = "Ed";
+const lastName = "Adeagbo";
+
+let age = 19;
+const y = age++;
+
+
+console.log(`Nice to meet you my name is ${firstName}`)
+
+console.log(`My full name is ${firstName} ${lastName}!`)
+
+console.log(`I will be ${age} in May, but I am ${y} right now.`);
+
+console.log(`I turn ${age} on May 24th`)
+
+console.log({firstName, lastName, age})
+
+
+
+
